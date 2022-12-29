6e0ce93c183aab1a7173481954b340b499758b86
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2022-12-29 17:19:57 +0900

    DELETE 불필요 주석 삭제
    - DELETE 불필요 주석 삭제

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,6 @@
-// src/redux/modules/config/configStore.js
-
 import { configureStore } from '@reduxjs/toolkit';
 import Post from '../modules/PostsSlice';
 
-// //
-//  * 모듈(Slice)이 여러개인 경우
-//  * 추가할때마다 reducer 안에 각 모듈의 slice.reducer를 추가해줘야 합니다.
 const store = configureStore({
   reducer: { Post: Post },
   //devtools 배포환경에선 사용못하도록 막기위함
